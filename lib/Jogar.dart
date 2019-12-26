9a49eb849a411a5a95e39225766495a277b96d6c
garfnadeck <serraopinto@gmail.com>
2019-12-26 15:33:34 -0400

    Initial commit

---
+++ lib/Jogar.dart
@@ -0,0 +1,44 @@
+import 'package:caraoucoroa/Resultado.dart';
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+class Jogar extends StatefulWidget {
+  @override
+  _JogarState createState() => _JogarState();
+}
+
+class _JogarState extends State<Jogar> {
+
+  void _exibirResultado() {
+
+    var itens = ['cara','coroa'];
+    var numero = Random().nextInt(2);
+    var resultado = itens[numero];
+
+
+    Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => Resultado(resultado)
+        ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff61bd86),
+      body: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset('images/logo.png'),
+            GestureDetector(
+              onTap: _exibirResultado,
+              child: Image.asset('images/botao_jogar.png'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:caraoucoroa/Jogar.dart';
+import 'package:flutter/material.dart';
+
+
+void main()=> runApp(MaterialApp(
+  home: Jogar(),
+
+));
